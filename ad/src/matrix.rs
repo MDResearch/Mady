b0a36d92e6c083c305441ac954af52a840a5d411
KAIYOHUGO <ky.mc.minecraft@gmail.com>
2021-12-15 12:34:28 +0800

    marge comment

--- ad/src/matrix.rs
+++ ad/src/matrix.rs
@@ -11,17 +11,10 @@ struct Matrix<T>
 where
     T: Copy,
 {
-<<<<<<< HEAD
-=======
->>>>>>> b7d17ceff0223c3f4f84a4e439c515bb38842a63
-data: Tensor<T>,
+    data: Tensor<T>,
     // 0 直排長度 1 橫排長度
     shape: [usize; 2],
 }
-
-
-
-
 
 impl<T> Matrix<T>
 where
@@ -34,11 +27,10 @@ where
         }
     }
 
-// transpose Matrix // 把Matrix轉掉
-// 1 2 3     1 4 
-// 4 5 6 ->  2 5 
-//           3 6
-
+    // transpose Matrix // 把Matrix轉掉
+    // 1 2 3     1 4
+    // 4 5 6 ->  2 5
+    //           3 6
 
     pub fn t(self) -> Transpose<T> {
         Transpose(self)
@@ -57,7 +49,6 @@ where
 
     // 從橫排掃過一遍
 
-
     // iter by col
     // it is non-consum iterator
     // 0  4  8
@@ -66,16 +57,15 @@ where
     // 3  7 11
     pub fn into_col_iter<'a>(&'a self) -> impl Iterator<Item = &T> + 'a {
         // 第1直排到第N直排的迭代器
-        (0..self.shape[1]) 
+        (0..self.shape[1])
             // flat_map 用來攤平 ex: [[1,2],[3,4],[5,6]] -> [1,2,3,4,5,6]
             // step_by(self.shape[1]) 跳直的 , take()用來取迭代器，相對的原本迭代器裡的那個iter會被刪掉 , b的值會來自前面的(0..self.shape[1]) , 也就是 第 0,1,2項的迭代器,是flat_map自己抓的
-            .flat_map(move |b| (b..).step_by(self.shape[1]).take(self.shape[0]))   
+            .flat_map(move |b| (b..).step_by(self.shape[1]).take(self.shape[0]))
             // map用來取迭代器的值 , map會自己抓上面flat_map跳到的位置n
-            .map(move |n| self.data.iter().nth(n).unwrap()) // 
+            .map(move |n| self.data.iter().nth(n).unwrap())
     }
 }
 
-<<<<<<< HEAD
 impl<T> From<(Tensor<T>, [usize; 2])> for Matrix<T>
 where
     T: Copy,
@@ -87,10 +77,6 @@ where
         }
     }
 }
-=======
-
-
->>>>>>> b7d17ceff0223c3f4f84a4e439c515bb38842a63
 
 impl<T> AsMut<Vec<T>> for Matrix<T>
 where
@@ -121,11 +107,10 @@ impl_ops_all!(+[<K, T> where T: Add<K> + Copy,K: Copy,<T as Add<K>>::Output: Cop
         Matrix {
             shape: left.shape.clone(),
             // 這裡可以直接+，來源自macros.rs
-            data: &left.data + &right.data, 
+            data: &left.data + &right.data,
         }
     }
 );
-
 
 // Add Matrix & Transpose
 impl_ops_all!(+[<K, T> where T: Add<K> + Copy,K: Copy,<T as Add<K>>::Output: Copy]
@@ -154,26 +139,6 @@ impl_ops_all!(-[<K, T> where T: Sub<K> + Copy,K: Copy,<T as Sub<K>>::Output: Cop
         }
     }
 );
-
-// Add Matrix & Transpose
-// impl<K, T> Add<Transpose<K>> for Matrix<T>
-// where
-//     T: Add<K> + Copy,
-//     K: Copy,
-//     <T as Add<K>>::Output: Copy,
-// {
-//     type Output = Matrix<<T as Add<K>>::Output>;
-//
-//     fn add(self, rhs: Transpose<K>) -> Self::Output {
-//         if cfg!(debug_assertions) {
-//             assert_eq!(self.shape, [rhs.0.shape[1],rhs.0.shape[0]])
-//         }
-//         Matrix {
-//             shape: rhs.shape.clone(),
-//             data: &self.data + &rhs.0.data,
-//         }
-//     }
-// }
 
 // Mul Matrix
 impl_ops_all!(*[<K,T> where T: Mul<K> + Copy,K: Copy,<T as Mul<K>>::Output: Copy + Default + Add<Output = <T as Mul<K>>::Output>]
@@ -303,70 +268,6 @@ impl_ops_all!(-[<K, T> where T: Sub<K> + Copy,K: Copy,<T as Sub<K>>::Output: Cop
         )
     }
 );
-
-// Add Transpose & Matrix
-//          3*2
-//   2*3   |1 4|
-// |1 2 3| |2 5|
-// |4 5 6| |3 6|
-// impl<K, T> Add<Matrix<K>> for Transpose<T>
-// where
-//     T: Add<K> + Copy,
-//     K: Copy,
-//     <T as Add<K>>::Output: Copy + Default,
-//     <T as Add<K>>::Output: Add<Output = <T as Add<K>>::Output>,
-// {
-//     type Output = Matrix<<T as Add<K>>::Output>;
-
-//     fn add(self, rhs: Matrix<K>) -> Self::Output {
-//         if cfg!(debug_assertions) {
-//             assert_eq!([self.0.shape[1], self.0.shape[0]], rhs.shape)
-//         }
-//         let mut data = Vec::new();
-//         let (mut rit, cit) = (self.into_row_iter(), rhs.into_col_iter());
-//         for r in 0..self.0.shape[0] {
-//             let cycle = rit.by_ref().take(self.0.shape[1]);
-//             for c in 0..rhs.shape[1] {
-//                 data.push(
-//                     cit.by_ref()
-//                         .zip(cycle)
-//                         .fold(<T as Add<K>>::Output::default(), |v, (a, b)| (b + a) + v),
-//                 );
-//             }
-//         }
-// (0..self.0.shape[0])
-//     .flat_map(|r| {
-//         (0..self.0.shape[1]).map(move |c| {
-//             rhs.into_col_iter()
-//                 .skip(c * self.0.shape[1])
-//                 .take(self.0.shape[1])
-//                 .zip((&self).into_row_iter().skip(r * self.0.shape[1]))
-//                 .map(|(a, b)| b + a)
-//                 .sum::<<T as Add<K>>::Output>()
-//         })
-//     })
-//     .collect::<<T as Add<K>>::Output>();
-// self.into_row_iter().take(self.0.shape[1]).zip(rhs.into_col_iter());
-// Matrix {
-//     shape: rhs.shape.clone(),
-//     data: todo!(),
-// }
-//         todo!()
-//     }
-// }
-// impl_ops_all!(+[<K, T> where T: Add<K> + Copy,K: Copy,<T as Add<K>>::Output: Copy]
-//     (left:Transpose<T>, right:Matrix<K>)->Matrix<<T as Add<K>>::Output>{
-//         // !for debug
-//         if cfg!(debug_assertions) {
-//             assert_eq!(left.0.shape, right.shape)
-//         }
-
-//         // Matrix {
-//         //     shape: left.0.shape.clone(),
-//         //     data: left.into_col_iter().into_iter().
-//         // }
-//     }
-// );
 
 #[cfg(test)]
 mod tests {
