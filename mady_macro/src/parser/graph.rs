c32ff67894ccdc7ffc216bd645fcc87ecf6a31a5
KAIYOHUGO <ky.mc.minecraft@gmail.com>
2022-02-20 09:13:58 +0800

    add assign (no test)

--- mady_macro/src/parser/graph.rs
+++ mady_macro/src/parser/graph.rs
@@ -8,6 +8,7 @@ use crate::graph::Graph;
 use proc_macro2::{token_stream, Ident, Span, TokenStream};
 use quote::{quote, ToTokens};
 use syn::fold::{fold_block, fold_expr, fold_expr_assign, fold_ident, Fold};
+use syn::token::Token;
 use syn::{
     parse2, parse_quote, parse_str, BinOp, Block, Expr, ExprAssign, ExprAssignOp, ItemFn, Local,
 };
@@ -85,18 +86,6 @@ impl Parser {
             .position(|&x| self.variables[x].hash == hash)
     }
 
-    // ! dont use
-    // fn link_nodes(&mut self, edge_var_id: usize, from_to: (usize, usize)) -> Result<(), ()> {
-    //     self.ad_graph.add_edge(
-    //         edge_var_id,
-    //         (
-    //             self.find_local(from_to.0).ok_or(())?,
-    //             self.find_local(from_to.1).ok_or(())?,
-    //         ),
-    //     );
-    //     Ok(())
-    // }
-
     /// enter a block
     ///
     /// push link
@@ -136,11 +125,16 @@ impl Parser {
         })
     }
 
-    fn build_graph(&mut self, e: Expr) -> Result<(usize, Expr), Expr> {
+    fn parse_expr(&mut self, e: Expr) -> Result<(usize, Expr), Expr> {
         match e {
             Expr::Binary(v) => {
-                let (left, left_expr) = self.build_graph(*v.left)?;
-                let (right, right_expr) = self.build_graph(*v.right)?;
+                let ops_ts = match parse_ops(v.op) {
+                    Ok(t) => t,
+                    Err(_) => return Err(Expr::Binary(v)),
+                };
+
+                let (left, left_expr) = self.parse_expr(*v.left)?;
+                let (right, right_expr) = self.parse_expr(*v.right)?;
                 let (edge_left_ident, edge_left) = self.new_tmp();
                 let (edge_right_ident, edge_right) = self.new_tmp();
                 let (_, ops) = self.new_tmp();
@@ -152,7 +146,7 @@ impl Parser {
                 let ts = parse_quote! {
                     {
                         let tmp;
-                        (tmp, (#edge_left_ident, #edge_right_ident)) = #left_expr.grad_add(#right_expr);
+                        (tmp, (#edge_left_ident, #edge_right_ident)) = #left_expr.#ops_ts(#right_expr);
                         tmp
                     }
                 };
@@ -166,8 +160,18 @@ impl Parser {
                     Err(Expr::Path(v))
                 }
             }
-            Expr::Let(v) => {
-                todo!()
+            Expr::Assign(v) => {
+                let (id, left) = self.parse_expr(*v.left)?;
+                let (top, right) = self.parse_expr(*v.right)?;
+                let (edge_ident, edge) = self.new_tmp();
+                self.ad_graph.add_edge(edge, (id, top));
+                let ts = parse_quote! {
+                    {
+                        #edge_ident = #right.one();
+                        #right = #left;
+                    }
+                };
+                Ok((id, ts))
             }
             _ => Err(fold_expr(self, e)),
         }
@@ -175,92 +179,8 @@ impl Parser {
 }
 
 impl Fold for Parser {
-    fn fold_expr_assign(&mut self, i: ExprAssign) -> ExprAssign {
-        todo!()
-    }
-
     fn fold_expr_assign_op(&mut self, i: ExprAssignOp) -> ExprAssignOp {
         todo!()
-    }
-
-    /// record varible
-    ///
-    /// ```
-    /// let (mut a, mut b, mut c)=(0, 0, 0);
-    /// a = b * c;
-    /// // covert to
-    /// a = {
-    ///     let mad_var_id = b * c;
-    ///     mad_var_id
-    /// };
-    /// ```
-    fn fold_expr(&mut self, i: Expr) -> syn::Expr {
-        match i {
-            Expr::Assign(stat) => {
-                let parent = *stat.left;
-
-                todo!()
-            }
-            Expr::Binary(v) => {
-                let op = v.op;
-
-                match op {
-                    BinOp::Add(_) => {
-                        panic!("deprecated");
-                        let left = self.fold_expr(*v.left);
-                        // let right = self.fold_expr(*v.right);
-
-                        // let (edge_left, edge_left_id) =
-                        //     self.push_var_key().expect("no stack to push");
-                        // let (edge_right, edge_right_id) =
-                        //     self.push_var_key().expect("no stack to push");
-                        // let (ops, ops_id) = self.push_var_key().expect("no stack to push");
-
-                        // let ops_node = self.ad_graph.add_node(ops_id);
-
-                        // // self.link_nodes(edge_right_id, (ops_node, 0));
-                        // // self.link_nodes(edge_left_id, (ops_node, 0));
-
-                        // parse_quote! {
-                        //     {
-                        //         let tmp;
-                        //         (tmp, (#edge_left, #edge_right)) = #left.grad_add(#right);
-                        //         tmp
-                        //     }
-                        // }
-                    }
-                    // BinOp::Sub(_) => {
-                    //     parse_quote! {
-                    //         {
-                    //             #left_var = #left.clone();
-                    //             #right_var = #right.clone();
-                    //             #left #op #right
-                    //         }
-                    //     }
-                    // }
-                    // BinOp::Mul(_) => {
-                    //     parse_quote! {
-                    //         {
-                    //             #left_var = #left.clone();
-                    //             #right_var = #right.clone();
-                    //             #left #op #right
-                    //         }
-                    //     }
-                    // }
-                    // BinOp::Div(_) => {
-                    //     parse_quote! {
-                    //         {
-                    //             #left_var = #left.clone();
-                    //             #right_var = #right.clone();
-                    //             #left #op #right
-                    //         }
-                    //     }
-                    // }
-                    _ => Expr::Binary(v),
-                }
-            }
-            _ => fold_expr(self, i),
-        }
     }
 
     fn fold_local(&mut self, i: Local) -> Local {
@@ -292,6 +212,16 @@ impl Fold for Parser {
     }
 }
 
+fn parse_ops(op: BinOp) -> Result<TokenStream, ()> {
+    match op {
+        BinOp::Add(_) => Ok(quote! {grad_add}),
+        BinOp::Sub(_) => Ok(quote! {grad_sub}),
+        BinOp::Mul(_) => Ok(quote! {grad_mul}),
+        BinOp::Div(_) => Ok(quote! {grad_div}),
+        _ => Err(()),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use syn::{parse2, Expr};
@@ -319,7 +249,7 @@ mod tests {
         let (.., b) = parser.new_local("b").unwrap();
         parser.ad_graph.add_node(a);
         parser.ad_graph.add_node(b);
-        let (top, ast) = parser.build_graph(ast).unwrap();
+        let (top, ast) = parser.parse_expr(ast).unwrap();
         assert_eq!(ast, res);
         assert_eq!(&top, parser.ad_graph.roots().first().unwrap());
     }
