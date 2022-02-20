0952b2ca0990e331d89e31f92890481da934b079
KAIYOHUGO <ky.mc.minecraft@gmail.com>
2022-02-20 21:46:28 +0800

    ⚡feat: add let , assign, pat support

--- mady_macro/src/parser/graph.rs
+++ mady_macro/src/parser/graph.rs
@@ -1,16 +1,15 @@
 use std::collections::{hash_map::DefaultHasher, LinkedList};
 use std::error::Error;
 use std::hash::{Hash, Hasher};
-use std::ops::Sub;
-use std::str::FromStr;
 
 use crate::graph::Graph;
-use proc_macro2::{token_stream, Ident, Span, TokenStream};
-use quote::{quote, ToTokens};
-use syn::fold::{fold_block, fold_expr, fold_expr_assign, fold_ident, Fold};
-use syn::token::Token;
+use proc_macro2::{Ident, Span, TokenStream};
+use quote::quote;
+use syn::fold::{
+    fold_block, fold_expr, fold_expr_assign, fold_ident, fold_local, fold_pat, fold_stmt, Fold,
+};
 use syn::{
-    parse2, parse_quote, parse_str, BinOp, Block, Expr, ExprAssign, ExprAssignOp, ItemFn, Local,
+    parse2, parse_quote, parse_str, BinOp, Block, Expr, ExprAssign, ItemFn, Local, Pat, Stmt,
 };
 
 #[derive(Debug, Default)]
@@ -80,10 +79,15 @@ impl Parser {
         let mut hasher = DefaultHasher::new();
         ident.hash(&mut hasher);
         let hash = hasher.finish();
-        self.stack
-            .last()?
-            .iter()
-            .position(|&x| self.variables[x].hash == hash)
+
+        for list in self.stack.iter().rev() {
+            for &index in list.iter().rev() {
+                if self.variables[index].hash == hash {
+                    return Some(index);
+                }
+            }
+        }
+        None
     }
 
     /// enter a block
@@ -124,17 +128,26 @@ impl Parser {
             }
         })
     }
+}
 
+impl Parser {
     fn parse_expr(&mut self, e: Expr) -> Result<(usize, Expr), Expr> {
         match e {
+            // a + b
+            //
+            // `a + b` is Expr::Binary
             Expr::Binary(v) => {
                 let ops_ts = match parse_ops(v.op) {
                     Ok(t) => t,
                     Err(_) => return Err(Expr::Binary(v)),
                 };
 
-                let (left, left_expr) = self.parse_expr(*v.left)?;
-                let (right, right_expr) = self.parse_expr(*v.right)?;
+                let (left, left_expr) = self
+                    .parse_expr(*v.left.clone())
+                    .map_err(|_| Expr::Binary(v.clone()))?;
+                let (right, right_expr) = self
+                    .parse_expr(*v.right.clone())
+                    .map_err(|_| Expr::Binary(v.clone()))?;
                 let (edge_left_ident, edge_left) = self.new_tmp();
                 let (edge_right_ident, edge_right) = self.new_tmp();
                 let (_, ops) = self.new_tmp();
@@ -152,6 +165,10 @@ impl Parser {
                 };
                 Ok((node_ops, ts))
             }
+
+            // a = b + c
+            //
+            // a, b, c is Expr::Path
             Expr::Path(v) => {
                 if let Some(ident) = v.path.get_ident() {
                     let id = self.find_local(ident).expect("not find varible in stack");
@@ -160,48 +177,118 @@ impl Parser {
                     Err(Expr::Path(v))
                 }
             }
+
+            // a = b
+            //
+            // `=` is Expr::Assign
             Expr::Assign(v) => {
-                let (id, left) = self.parse_expr(*v.left)?;
-                let (top, right) = self.parse_expr(*v.right)?;
+                let (id, left) = self
+                    .parse_expr(*v.left.clone())
+                    .map_err(|_| Expr::Assign(v.clone()))?;
+                let (top, right) = self
+                    .parse_expr(*v.right.clone())
+                    .map_err(|_| Expr::Assign(v.clone()))?;
                 let (edge_ident, edge) = self.new_tmp();
                 self.ad_graph.add_edge(edge, (id, top));
-                let ts = parse_quote! {
+                let right = parse_quote! {
                     {
-                        #edge_ident = #right.one();
-                        #right = #left;
+                        #edge_ident = #left.one();
+                        #right
                     }
                 };
-                Ok((id, ts))
+                let ts = ExprAssign {
+                    left: Box::new(left.clone()),
+                    right: Box::new(right),
+                    ..v
+                };
+
+                Ok((id, Expr::Assign(ts)))
             }
+
             _ => Err(fold_expr(self, e)),
+        }
+    }
+
+    fn parse_pat(&mut self, e: Pat) -> Result<(usize, Pat), Pat> {
+        match e {
+            // let a;
+            //
+            // a is Pat::Ident
+            //
+            // expect some bug
+            Pat::Ident(v) => {
+                if let Ok((.., id)) = self.new_local(v.ident.to_string()) {
+                    Ok((self.ad_graph.add_node(id), Pat::Ident(v)))
+                } else {
+                    Err(Pat::Ident(v))
+                }
+            }
+            _ => Err(fold_pat(self, e)),
+        }
+    }
+
+    fn parse_stmt(&mut self, e: Stmt) -> Result<(usize, Stmt), Stmt> {
+        match e {
+            Stmt::Local(v) => {
+                let (id, left) = self
+                    .parse_pat(v.pat.clone())
+                    .map_err(|_| Stmt::Local(v.clone()))?;
+                if let Some((eq, expr)) = v.init.clone() {
+                    let (top, right) =
+                        self.parse_expr(*expr).map_err(|_| Stmt::Local(v.clone()))?;
+                    let (edge_ident, edge) = self.new_tmp();
+                    self.ad_graph.add_edge(edge, (id, top));
+                    let right=parse_quote!{
+                        {
+                            #edge_ident = #left.one();
+                            #right
+                        }
+                    };
+                    let ts= Local {
+                        pat: left,
+                        init: Some((eq, Box::new(right))),
+                        ..v
+                    };
+                    Ok((id, Stmt::Local(ts)))
+                } else {
+                    Ok((id, Stmt::Local(Local { pat: left, ..v })))
+                }
+            }
+
+            // todo `const`
+            Stmt::Item(v) => todo!(),
+            Stmt::Expr(v) => self
+                .parse_expr(v)
+                .map(|(id, expr)| (id, Stmt::Expr(expr)))
+                .map_err(|expr| Stmt::Expr(expr)),
+            Stmt::Semi(v, t) => match self.parse_expr(v) {
+                Ok((id, expr)) => Ok((id, Stmt::Semi(expr, t))),
+                Err(expr) => Err(Stmt::Semi(expr, t)),
+            },
         }
     }
 }
 
 impl Fold for Parser {
-    fn fold_expr_assign_op(&mut self, i: ExprAssignOp) -> ExprAssignOp {
-        todo!()
+    fn fold_pat(&mut self, i: Pat) -> Pat {
+        match self.parse_pat(i) {
+            Ok((.., v)) => v,
+            Err(v) => v,
+        }
     }
 
-    fn fold_local(&mut self, i: Local) -> Local {
-        for attr in i.attrs {
-            // something like this
-            //
-            // ```
-            // #[grad]
-            // a = 10;
-            // ```
-            if attr.path.is_ident("grad") {
-                // add grad
-                match i.pat {
-                    syn::Pat::Ident(_) => todo!(),
-                    _ => {}
-                }
-                unimplemented!();
-                break;
-            }
+    fn fold_expr(&mut self, i: Expr) -> Expr {
+        match self.parse_expr(i) {
+            Ok((.., v)) => v,
+            Err(v) => v,
         }
-        todo!()
+    }
+
+    fn fold_stmt(&mut self, i: Stmt) -> Stmt {
+        match self.parse_stmt(i) {
+            Ok((.., v)) => v,
+            Err(v) => v,
+        }
     }
 
     fn fold_block(&mut self, i: Block) -> Block {
@@ -252,6 +339,97 @@ mod tests {
         let (top, ast) = parser.parse_expr(ast).unwrap();
         assert_eq!(ast, res);
         assert_eq!(&top, parser.ad_graph.roots().first().unwrap());
+    }
+
+    #[test]
+    fn test_expr_assign() {
+        let s = quote! {
+            {
+                c = a - b;
+            }
+        };
+        let ast: Expr = parse2(s).expect("unknow tokenstream");
+        let s = quote! {
+            {
+                c = {
+                    mad_var_6 = c.one();
+                    {
+                        let tmp;
+                        (tmp, (mad_var_3, mad_var_4)) = a.grad_sub(b);
+                        tmp
+                    }
+                };
+            }
+        };
+        let res: Expr = parse2(s).expect("unknow tokenstream");
+        let mut parser = Parser::new();
+        parser.enter_block();
+        let (.., a) = parser.new_local("a").unwrap();
+        let (.., b) = parser.new_local("b").unwrap();
+        let (.., c) = parser.new_local("c").unwrap();
+        parser.ad_graph.add_node(a);
+        parser.ad_graph.add_node(b);
+        parser.ad_graph.add_node(c);
+        let ast = parser.fold_expr(ast);
+        assert_eq!(ast, res);
+    }
+
+    #[test]
+    fn test_expr_local_declare() {
+        let s = quote! {
+            {
+                let (a, b)=(1, 2);
+                let c;
+                c = a * b;
+            }
+        };
+        let ast: Expr = parse2(s).expect("unknow tokenstream");
+        let s = quote! {
+            {
+                let (a, b)=(1, 2);
+                let c;
+                c = {
+                    mad_var_6 = c.one();
+                    {
+                        let tmp;
+                        (tmp, (mad_var_3, mad_var_4)) = a.grad_mul(b);
+                        tmp
+                    }
+                };
+            }
+        };
+        let res: Expr = parse2(s).expect("unknow tokenstream");
+        let mut parser = Parser::new();
+        let ast = parser.fold_expr(ast);
+        assert_eq!(ast, res);
+    }
+
+    #[test]
+    fn test_expr_local_assign() {
+        let s = quote! {
+            {
+                let (a, b)=(1, 2);
+                let c = a / b;
+            }
+        };
+        let ast: Expr = parse2(s).expect("unknow tokenstream");
+        let s = quote! {
+            {
+                let (a, b)=(1, 2);
+                let c = {
+                    mad_var_6 = c.one();
+                    {
+                        let tmp;
+                        (tmp, (mad_var_3, mad_var_4)) = a.grad_div(b);
+                        tmp
+                    }
+                };
+            }
+        };
+        let res: Expr = parse2(s).expect("unknow tokenstream");
+        let mut parser = Parser::new();
+        let ast = parser.fold_expr(ast);
+        assert_eq!(ast, res);
     }
 }
 
