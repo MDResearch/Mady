e9ea7c18ea7e1366cba9ea9e9adb8a70f1026bf4
KAIYOHUGO <ky.mc.minecraft@gmail.com>
2022-03-09 16:47:56 +0800

    change to more rusty way to impl trait

--- mady_math/src/lib.rs
+++ mady_math/src/lib.rs
@@ -6,6 +6,8 @@ mod macros;
 
 pub mod prelude {
     pub use super::grad::*;
+    pub use super::grad::impl_one as _;
+    pub use super::grad::impl_zero as _;
     pub use super::matrix::{mat, Matrix};
     pub use super::tensor::{ten, Tensor};
 }
