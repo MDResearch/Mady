112794f2679136bf871c7b1f068243033ebd215f
Eason0729 <easonqq0000@gmail.com>
2022-03-10 22:55:51 +0800

    mul

--- mady_math/src/ndarray.rs
+++ mady_math/src/ndarray.rs
@@ -1,16 +1,20 @@
 use super::grad::Zero;
 use std::{
     marker::PhantomData,
-    ops::{Add, Mul},
+    ops::{Add, Div, Mul, Neg, Sub},
 };
 
+#[derive(Debug)]
 struct NDArray<T, D> {
     phantom: PhantomData<D>,
     data: Vec<T>,
     size: Vec<usize>,
 }
 
+#[derive(Debug)]
 struct D1;
+
+#[derive(Debug)]
 struct D2;
 
 impl<T> NDArray<T, D1>
@@ -26,12 +30,14 @@ where
     }
 
     fn dot(self: Self, i: Self) -> T {
-        return self
-            .data
+        if cfg!(debug_assertions) {
+            assert_eq!(self.size, i.size);
+        }
+        self.data
             .iter()
             .zip(i.data.iter())
             .map(|(&x, &y)| x * y)
-            .fold(Zero::zero(), |a, b| a + b);
+            .fold(Zero::zero(), |a, b| a + b)
     }
 
     fn cross(self: Self, i: Self) -> NDArray<T, D1> {
@@ -39,21 +45,83 @@ where
         // https://en.wikipedia.org/wiki/Matrix_calculus
 
         if cfg!(debug_assertions) {
-            assert_eq!(self.size, i.size)
+            assert_eq!(self.size, i.size);
         }
-
         todo!();
     }
 }
 
+impl<T> NDArray<T, D2>
+where
+    T: Mul<Output = T> + Add<Output = T> + Zero + Copy,
+{
+    fn new(data: Vec<T>, size: (usize, usize)) -> Self {
+        if cfg!(debug_assertions) {
+            assert_eq!(size.0 * size.1, data.len());
+        }
+        Self {
+            phantom: PhantomData,
+            size: vec![size.0, size.1],
+            data,
+        }
+    }
+
+    fn mul(self: Self, i: NDArray<T, D1>) -> NDArray<T, D1> {
+        let mut result = vec![];
+        if cfg!(debug_assertions) {
+            assert_eq!(self.size[0], i.size[0]);
+        }
+        for j in 0..self.size[1] {
+            let mut sum = Zero::zero();
+            for c in 0..self.size[0] {
+                sum = sum + self.data[j * self.size[0] + c] * i.data[c];
+            }
+            result.push(sum);
+        }
+
+        NDArray::<T, D1>::new(result)
+    }
+
+    fn grad_mul(
+        self: Self,
+        i: NDArray<T, D1>,
+    ) -> (NDArray<T, D1>, (NDArray<T, D2>, NDArray<T, D1>)) {
+        let mut result = vec![];
+        if cfg!(debug_assertions) {
+            assert_eq!(self.size[0], i.size[0]);
+        }
+        for j in 0..self.size[1] {
+            let mut sum: T = Zero::zero();
+            for c in 0..self.size[0] {
+                sum = sum + self.data[j * self.size[0] + c] * i.data[c];
+            }
+            result.push(sum);
+        }
+
+        NDArray::<T, D1>::new(result);
+        todo!()
+    }
+}
+
 mod tests {
-    use super::NDArray;
+    use super::{NDArray, D1, D2};
 
     #[test]
     fn d1_dot() {
-        let vec_a = NDArray::new(vec![1, 2, 3]);
-        let vec_b = NDArray::new(vec![6, 7, 3]);
+        let vec_a = NDArray::<i32, D1>::new(vec![1, 2, 3]);
+        let vec_b = NDArray::<i32, D1>::new(vec![6, 7, 3]);
         let result = vec_a.dot(vec_b);
+
         assert_eq!(result, 29 as i32);
+    }
+
+    #[test]
+    fn d2_mul_d1() {
+        let vec_a = NDArray::<i32, D2>::new(vec![1, 2, 3, 4], (2, 2));
+        let vec_b = NDArray::<i32, D1>::new(vec![7, 1]);
+        let result = vec_a.mul(vec_b);
+
+        assert_eq!(result.data[0], 9);
+        assert_eq!(result.data[1], 25);
     }
 }
