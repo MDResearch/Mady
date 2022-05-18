61331f217a995f0542846acb192c3b1330cd4db1
CI <ci@mady.ci>
2022-05-18 02:04:09 +0000

    FMT & Autofix

--- mady_macro/src/lib.rs
+++ mady_macro/src/lib.rs
@@ -3,7 +3,7 @@ use quote::quote;
 use syn::{parse_macro_input, ItemFn};
 
 /// this scope describe how to use this library
-/// 
+///
 /// "grad" is a macro which let you get to the differentiation of the formula you inputed
 ///
 ///  the usage of [grad] attribute will be introduced below
@@ -14,14 +14,14 @@ use syn::{parse_macro_input, ItemFn};
 ///     a + b
 /// }
 ///  ```
-/// 
-/// The 1st line decribe the method of importing grad 
+///
+/// The 1st line decribe the method of importing grad
 ///  
-/// The 2nd line describe a function,and this function include the formula to be different. 
+/// The 2nd line describe a function,and this function include the formula to be different.
 /// The parameter involve which Immutable number used in the formula
-/// 
+///
 /// The third line decribe the formula you want to different; for this instance, it different a `a + b` formula
-/// 
+///
 #[proc_macro_attribute]
 pub fn grad(_attr: TokenStream, input: TokenStream) -> TokenStream {
     let mut parser = mady_macro_core::new();
