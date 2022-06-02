15a96b69d3879b4ec259a8dae8838519f533210a
KAIYOHUGO <ky.mc.minecraft@gmail.com>
2022-06-02 20:37:19 +0800

    🔕(🦀) Remove Unuse Rexport

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@ pub mod prelude {
     pub use mady_macro::*;
     #[cfg(feature = "math")]
     pub use mady_math::prelude::*;
-    #[cfg(feature = "macro")]
-    pub use std::ops::Mul;
 }
 
 #[cfg(feature = "macro")]
